e27a46ceefd52a1908c545371fe04dcfb2e6bc36
lordinicloud <166272154+lordinicloud@users.noreply.github.com>
2024-04-07 14:08:24 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -12,6 +12,7 @@ let addresses = [
 let addressesapi = [
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
+	'https://resourcecore-optimization-pages.pages.dev/ip.txt?token=e03ee7ddf90f',
 ];
 
 // 设置优选地址，不带端口号默认80，noTLS订阅生成
